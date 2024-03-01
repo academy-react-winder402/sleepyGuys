25255295c8cf28d697078826934e062f9a659d69
micheal aghaei <aghaeimicheal@gmail.com>
2024-03-01 20:53:39 +0330

    init route

---
+++ pages/blogs/[blog-name]/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BlogName() {
+  return (
+    <div>BlogName</div>
+  )
+}
+
+export default BlogName
---
+++ pages/teachers/[teacher-name]/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TeacherName() {
+  return (
+    <div>TeacherName</div>
+  )
+}
+
+export default TeacherName
