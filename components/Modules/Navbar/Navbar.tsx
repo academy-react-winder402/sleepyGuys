c89f25476f528889048a70ea765ba9566592fefc
Aryan kholqi <aryankholqi@gmail.com>
2024-03-02 09:44:05 +0330

    develop navbar

--- components/Modules/Navbar/Navbar.tsx
+++ components/Modules/Navbar/Navbar.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import navbarLogo from "@/public/icons/logo/navbarLogo.svg";
 import Link from "next/link";
 import { Avatar, AvatarIcon } from "@nextui-org/avatar";
+import { Switch } from "@nextui-org/react";
+import moonIcon from "@/public/icons/theme/moon.svg";
+import sunIcon from "@/public/icons/theme/sun.svg";
 
 function Navbar() {
+  const [isSelected, setIsSelected] = useState(true);
+
   return (
     <nav className="flex items-center justify-between">
       <div className="flex items-center gap-x-6">
@@ -27,7 +32,28 @@ function Navbar() {
           </li>
         </ul>
       </div>
-      <Avatar />
+      <div className="flex flex-row-reverse items-center gap-x-2" dir="ltr">
+        <Switch
+          isSelected={isSelected}
+          onValueChange={setIsSelected}
+          size="lg"
+          color="default"
+          thumbIcon={({ isSelected }) =>
+            isSelected ? (
+              <Image src={sunIcon} alt="" />
+            ) : (
+              <Image src={moonIcon} alt="" />
+            )
+          }
+        />
+        <Avatar
+          icon={<AvatarIcon />}
+          classNames={{
+            base: "bg-[#436E8E]",
+            icon: "text-white",
+          }}
+        />
+      </div>
     </nav>
   );
 }
