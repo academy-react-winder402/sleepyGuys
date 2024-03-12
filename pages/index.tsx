42ff7c9d5ac147e133470c0a324ef6f12ff8c463
Aryan kholqi <aryankholqi@gmail.com>
2024-03-13 01:46:42 +0330

    fix vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Hero from "@/components/Templates/Landing/Hero/Hero";
+import Hero from "@/components/Templates/Landing/Hero/HeroBox";
 import React from "react";
 
 export default function Home() {
