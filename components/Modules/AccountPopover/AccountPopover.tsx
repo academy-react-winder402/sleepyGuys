9435158df80e2ba20938e3068e5a04b98da8d70e
Aryan kholqi <aryankholqi@gmail.com>
2024-03-13 01:19:09 +0330

    few changes

---
+++ components/Modules/AccountPopover/AccountPopover.tsx
@@ -0,0 +1,77 @@
+import accountPopoverItems from "@/constants/accountPopoverItems";
+import {
+  Divider,
+  Listbox,
+  ListboxItem,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import Image from "next/image";
+import React from "react";
+import logout from "@/public/icons/theme/logout.svg";
+import userIcon from "@/public/icons/theme/user.svg";
+import { useRouter } from "next/router";
+
+export default function AccountPopover() {
+  const router = useRouter();
+
+  return (
+    <Popover offset={15} placement="bottom-start" backdrop={"blur"}>
+      <PopoverTrigger>
+        <div className="flex items-center justify-center w-10 h-10 md:w-11 md:h-11 bg-primary rounded-full">
+          <Image
+            className="scale-85 md:scale-100"
+            src={userIcon}
+            alt=""
+            width={28}
+            height={12}
+          />
+        </div>
+      </PopoverTrigger>
+      <PopoverContent className="w-[340px] items-start rounded-tl-none p-6">
+        <div className="flex gap-x-4">
+          <div className="flex items-center justify-center w-12 h-12 md:w-14 md:h-14 bg-primary rounded-full">
+            <Image
+              className="scale-85 md:scale-100"
+              src={userIcon}
+              alt=""
+              width={38}
+              height={18}
+            />
+          </div>
+          <div>
+            <p className="text-lg font-bold text-primary mb-1">
+              آرمان غنی زاده
+            </p>
+            <p className="text-xs text-black-500">موجودی : ۰ تومان</p>
+          </div>
+        </div>
+        <Divider className="my-3" />
+        <div className="w-full flex flex-col">
+          <Listbox onAction={(key) => router.push(`/${key}`)}>
+            {accountPopoverItems.map((item) => (
+              <ListboxItem key={item.route} className="py-4">
+                <div className="flex items-center gap-x-3">
+                  <Image src={item.icon} alt="" width={24} height={24} />
+                  <p className="text-sm text-black-500">{item.label}</p>
+                </div>
+              </ListboxItem>
+            ))}
+          </Listbox>
+          <Divider className="my-4" />
+          <div className="flex items-center justify-center gap-x-1 text-md font-bold">
+            <Image
+              className="scale-85"
+              src={logout}
+              alt=""
+              width={30}
+              height={30}
+            />
+            <p>خروج از حساب </p>
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+}
--- components/Modules/Navbar/Navbar.tsx
+++ components/Modules/Navbar/Navbar.tsx
@@ -2,22 +2,11 @@ import Image from "next/image";
 import React from "react";
 import navbarLogo from "@/public/icons/logo/navbarLogo.svg";
 import Link from "next/link";
-import {
-  Divider,
-  Listbox,
-  ListboxItem,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@nextui-org/react";
-import logout from "@/public/icons/theme/logout.svg";
-import userIcon from "@/public/icons/theme/user.svg";
-import accountPopoverItems from "@/constants/accountPopoverItems";
-import { useRouter } from "next/router";
-import ThemeSwitch from "@/components/Modules/Navbar/theme";
+import ThemeSwitch from "@/components/Modules/Navbar/ThemeSwitch";
 import { usePathname } from "next/navigation";
 import NavbarInPhone from "./NavbarInPhone";
 import navbarRoutesItems from "@/constants/navbarRoutesItems";
+import AccountPopover from "../AccountPopover/AccountPopover";
 
 function Navbar() {
   // " Ino chhera gozashti? "
@@ -26,8 +15,6 @@ function Navbar() {
   // useEffect(() => {
   //   setWindowWidth(typeof window !== "undefined" ? window.innerWidth : 0);
   // }, [typeof window !== "undefined" ? window.innerWidth : 0]);
-
-  const router = useRouter();
 
   const pathname = usePathname();
 
@@ -59,62 +46,7 @@ function Navbar() {
           dir="ltr"
         >
           <ThemeSwitch />
-          <Popover offset={15} placement="bottom-start" backdrop={"blur"}>
-            <PopoverTrigger>
-              <div className="flex items-center justify-center w-10 h-10 md:w-11 md:h-11 bg-primary rounded-full">
-                <Image
-                  className="scale-85 md:scale-100"
-                  src={userIcon}
-                  alt=""
-                  width={28}
-                  height={12}
-                />
-              </div>
-            </PopoverTrigger>
-            <PopoverContent className="w-[340px] items-start rounded-tl-none p-6">
-              <div className="flex gap-x-4">
-                <div className="flex items-center justify-center w-12 h-12 md:w-14 md:h-14 bg-primary rounded-full">
-                  <Image
-                    className="scale-85 md:scale-100"
-                    src={userIcon}
-                    alt=""
-                    width={38}
-                    height={18}
-                  />
-                </div>
-                <div>
-                  <p className="text-lg font-bold text-primary mb-1">
-                    آرمان غنی زاده
-                  </p>
-                  <p className="text-xs text-black-500">موجودی : ۰ تومان</p>
-                </div>
-              </div>
-              <Divider className="my-3" />
-              <div className="w-full flex flex-col">
-                <Listbox onAction={(key) => router.push(`/${key}`)}>
-                  {accountPopoverItems.map((item) => (
-                    <ListboxItem key={item.route} className="py-4">
-                      <div className="flex items-center gap-x-3">
-                        <Image src={item.icon} alt="" width={24} height={24} />
-                        <p className="text-sm text-black-500">{item.label}</p>
-                      </div>
-                    </ListboxItem>
-                  ))}
-                </Listbox>
-                <Divider className="my-4" />
-                <div className="flex items-center justify-center gap-x-1 text-md font-bold">
-                  <Image
-                    className="scale-85"
-                    src={logout}
-                    alt=""
-                    width={30}
-                    height={30}
-                  />
-                  <p>خروج از حساب </p>
-                </div>
-              </div>
-            </PopoverContent>
-          </Popover>
+          <AccountPopover />
         </div>
       </nav>
       <nav className="flex justify-center sm:hidden fixed bottom-3 px-3 w-full">
