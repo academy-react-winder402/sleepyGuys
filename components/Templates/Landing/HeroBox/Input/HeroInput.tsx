60e9e3c351dc3559421d3aff9a9c9634ca7c6832
Aryan kholqi <aryankholqi@gmail.com>
2024-03-14 17:21:21 +0330

    few changes on Hero input

--- components/Templates/Landing/HeroBox/Input/HeroInput.tsx
+++ components/Templates/Landing/HeroBox/Input/HeroInput.tsx
@@ -5,7 +5,6 @@ function HeroInput() {
   return (
     <>
       <Input
-        isClearable
         radius="lg"
         classNames={{
           input: [
@@ -13,13 +12,13 @@ function HeroInput() {
             "bg-transparent",
             "text-black/90 dark:text-white/90",
             "placeholder:text-default-700/50 dark:placeholder:text-white/60",
+            "font-vazir",
           ],
-          mainWrapper: "rounded-[20px]  overflow-hidden",
-          innerWrapper: "bg-transparent",
+          mainWrapper: ["rounded-[20px]", "overflow-hidden"],
+          innerWrapper: ["bg-transparent"],
           inputWrapper: [
             "shadow-xl",
             "bg-white",
-            // "bg-default-200/50",
             "dark:bg-default/60",
             "backdrop-blur-xl",
             "backdrop-saturate-200",
