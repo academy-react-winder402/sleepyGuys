82fa29eb6b8d9c9d72f3d931f1c58c0c8570a51f
micheal aghaei <aghaeimicheal@gmail.com>
2024-03-18 19:29:54 +0330

    fix news section

--- components/Modules/Slider/Slider.tsx
+++ components/Modules/Slider/Slider.tsx
@@ -5,6 +5,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCards } from "swiper/modules";
 import { getNews } from "@/mock/getNews";
 import NewsCard from "./Contents/NewsCard";
+import Link from "next/link";
 
 export default function Slider() {
   return (
@@ -13,13 +14,16 @@ export default function Slider() {
       effect={"cards"}
       grabCursor={true}
       modules={[EffectCards]}
-      className="w-[90%] sm:w-[92%] md:w-[85%] xl:w-[75%] sm:h-[250px] md:h-[280px] lg:h-[350px]"
+      className="w-[90%] sm:w-[92%] md:w-[85%] xl:w-[75%] sm:h-[230px] md:h-[280px] lg:h-[320px]"
     >
       {getNews().map((news , index) => (
         <SwiperSlide  key={index} className="bg-white-100">
           <NewsCard {...news} />
         </SwiperSlide>
       ))}
+      <Link href={"/blogs"} className="w-full block text-left -mt-8 text-black-400">
+      <span className="text-black-700">------</span> {" "}{" "}مشاهده همه اخبار
+      </Link>
     </Swiper>
   );
 }
--- components/Templates/Landing/NewsBox/NewsBox.tsx
+++ components/Templates/Landing/NewsBox/NewsBox.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export default function NewsBox() {
   return (
-    <div className="lg:mt-36 mt-24 text-center">
-      <h1 className="font-peyda text-[30px] md:text-[35px] text-primary dark:text-primary-lighter lg:mb-10 mb-8">
+    <div className="lg:mt-36 mt-24">
+      <h1 className="font-peyda text-[30px] md:text-[35px] text-primary dark:text-primary-lighter lg:mb-10 mb-8 text-center">
         آخرین اخبار
       </h1>
       <Slider />
