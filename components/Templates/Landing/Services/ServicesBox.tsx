ef006cb9ca5929d56bb923509657cd71f1bdcb57
Aryan kholqi <aryankholqi@gmail.com>
2024-03-19 11:09:54 +0330

    rename files

--- components/Templates/Landing/Services/ServicesBox.tsx
+++ components/Templates/Landing/Services/ServicesBox.tsx
@@ -1,7 +1,7 @@
 import Slider from "@/components/Modules/Slider/Slider";
 import React from "react";
 
-export default function ServiceBox() {
+export default function ServicesBox() {
   return (
     <div className="lg:mt-36 mt-24">
       <h1 className="font-peyda text-[30px] md:text-[35px] text-primary dark:text-primary-lighter lg:mb-10 mb-8 text-center">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
 import Categoris from "@/components/Templates/Landing/Categories/Categories";
-import HeroBox from "@/components/Templates/Landing/HeroBox/HeroBox";
-import NewCoursesBox from "@/components/Templates/Landing/NewCoursesBox/NewCoursesBox";
-import NewsBox from "@/components/Templates/Landing/NewsBox/NewsBox";
-import ServiceBox from "@/components/Templates/Landing/ServiceBox/ServiceBox";
+import HeroBox from "@/components/Templates/Landing/Hero/HeroBox";
+import NewCoursesBox from "@/components/Templates/Landing/NewCourses/NewCoursesBox";
+import NewsBox from "@/components/Templates/Landing/News/NewsBox";
+import ServiceBox from "@/components/Templates/Landing/Services/ServicesBox";
 import React from "react";
 
 export default function Home() {
   return (
     <>
-      <HeroBox/>
-      <Categoris/>
-      <NewCoursesBox/>
-      <NewsBox/>
-      <ServiceBox/>
+      <HeroBox />
+      <Categoris />
+      <NewCoursesBox />
+      <NewsBox />
+      <ServiceBox />
     </>
   );
 }
