276823cfba61966e177197e0d87b4a2789d47340
Aryan kholqi <aryankholqi@gmail.com>
2024-03-28 18:29:27 +0330

    few changes on map folder structure

--- components/Templates/Contact/Map/Map.tsx
+++ components/Templates/Contact/Map/Map.tsx
@@ -1,51 +1,26 @@
-'use client'
-import L from 'leaflet'
-import MarkerIcon from '@/public/icons/map/images/marker-icon.png'
-import MarkerShadow from '@/public/icons/map/images/marker-shadow.png'
-import 'leaflet/dist/leaflet.css'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import { useState } from 'react'
+import BoxHeader from "@/components/Modules/BoxHeader/BoxHeader";
+import dynamic from "next/dynamic";
 
+const DynamicMap = dynamic(() => import("@/components/Modules/Map/Map"), {
+  ssr: false,
+});
 
-const Map = () => {
-    const [coord, setCoord] = useState<[number , number]>([36.59700, 53.06255])
-    
-    return (
-        <div className='bg-MainPrimary overflow-hidden rounded-3xl'>
-       
-            <MapContainer style={{
-                height: '50vh',
-                width: '100%',
-                backgroundColor: '#436E8E',
-                opacity: 0.9,
-            }} center={coord} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
+const MapBox = () => {
+  return (
+    <>
+      <BoxHeader
+        title="یه سر بیا پیشمون"
+        hasLink={false}
+        linkTitle=""
+        route=""
+      />
+      <DynamicMap />
+      <p className="text-LightBody dark:text-DarkBody flex justify-center py-2 text-sm">
+        آدرس ما ساری بلوار خزر آموزشگاه دکتر بحرالعلوم هست.منتظر حضور گرمتان
+        هستیم .
+      </p>
+    </>
+  );
+};
 
-                <Marker icon={
-                    new L.Icon({
-                        iconUrl: MarkerIcon.src,
-                        iconRetinaUrl: MarkerIcon.src,
-                        iconSize: [25, 41],
-                        iconAnchor: [12.5, 41],
-                        popupAnchor: [0, -41],
-                        shadowUrl: MarkerShadow.src,
-                        shadowSize: [41, 41],
-                    })
-                } position={[36.59704, 53.06460]}>
-                     <Popup>
-                        <p className='text-MainPrimary'>آکادمی سپهر</p>
-                    </Popup>
-                </Marker>
-            </MapContainer>
-        </div>
-    )
-}
-
-export default Map
-
-
-
-
+export default MapBox;
--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,32 +1,11 @@
-import Map from "@/components/Templates/Contact/Map/Map";
+import MapBox from "@/components/Templates/Contact/Map/Map";
 import React from "react";
-import BoxHeader from "@/components/Modules/BoxHeader/BoxHeader";
-
-import dynamic from "next/dynamic";
-import Footer from "@/components/Modules/Footer/Footer";
-
-const DynamicMap = dynamic(
-  () => import("@/components/Templates/Contact/Map/Map"),
-  {
-    ssr: false,
-  }
-);
 
 function Contact() {
   return (
-    <div>
-      <BoxHeader
-        title="یه سر بیا پیشمون"
-        hasLink={false}
-        linkTitle=""
-        route=""
-      />
-      <DynamicMap />
-      <p className="text-LightBody dark:text-DarkBody flex justify-center py-2 text-sm">
-        آدرس ما ساری بلوار خزر آموزشگاه دکتر بحرالعلوم هست.منتظر حضور گرمتان
-        هستیم .
-      </p>
-    </div>
+    <>
+      <MapBox />
+    </>
   );
 }
 
