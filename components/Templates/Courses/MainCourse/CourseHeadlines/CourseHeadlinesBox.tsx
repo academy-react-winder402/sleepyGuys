2197b5ef4907875d107f3c4f6264806cf93e3329
micheal aghaei <aghaeimicheal@gmail.com>
2024-04-01 01:32:37 +0330

    compelete course headline

--- components/Templates/Courses/MainCourse/CourseHeadlines/CourseHeadlinesBox.tsx
+++ components/Templates/Courses/MainCourse/CourseHeadlines/CourseHeadlinesBox.tsx
@@ -1,15 +1,9 @@
-import MainButton from "@/components/Modules/Button/MainButton";
 import { Card, CardBody, CardHeader, ScrollShadow } from "@nextui-org/react";
-import Image from "next/image";
 import React, { useState } from "react";
 import { Accordion, AccordionItem } from "@nextui-org/react";
 import { CourseTitle } from "@/interfaces/courseTitleItem.interface";
 
 export default function CourseHeadlinesBox({ data }: { data: CourseTitle[] }) {
-  console.log(data);
-  const defaultContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-
   return (
     <Card
       className="shadow-2xl shadow-shadowColor dark:shadow-none p-6 dark:bg-dark-lighter"
