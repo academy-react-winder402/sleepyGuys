0515bab5c5c20662f92ebd982e499bfbdcea6282
Aryan kholqi <aryankholqi@gmail.com>
2024-04-02 22:12:08 +0330

    few changes

--- components/Modules/BoxHeader/BoxHeader.tsx
+++ components/Modules/BoxHeader/BoxHeader.tsx
@@ -11,16 +11,16 @@ export default function BoxHeader({
 }: BoxHeader) {
   return (
     <div className="flex-grow items-center grid grid-cols-1 md:grid-cols-3 justify-between">
-      <h1 className="font-peyda text-[30px] md:text-[35px] text-primary dark:text-primary-lighter lg:mb-10 mb-8 text-center md:col-start-2 col-span-1 mx-auto">
+      <h1 className="font-peyda text-[30px] md:text-[35px] text-primary dark:text-primary-lighter mb-2 md:mb-4 lg:mb-10  text-center md:col-start-2 col-span-1 mx-auto">
         {title}
       </h1>
       {hasLink && (
         <Link
           href={`${route}`}
-          className="md:col-start-3 col-span-1 mx-auto md:ms-auto md:mx-0 flex items-center gap-x-2 lg:mb-10 mb-8 text-lightTitle dark:text-darkTitle"
+          className="md:col-start-3 col-span-1 mx-auto md:ms-auto md:mx-0 flex items-center gap-x-2 mb-8 md:mb-4 lg:mb-10 text-lightTitle dark:text-darkTitle"
         >
-          {linkTitle}
           <Divider className="w-[30px] h-1 bg-primary dark:bg-primary-lighter" />
+          {linkTitle}
         </Link>
       )}
     </div>
