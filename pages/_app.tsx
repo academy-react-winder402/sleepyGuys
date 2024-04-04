344bf475f6c2a7a479c93412c60eec2d63053679
Aryan kholqi <aryankholqi@gmail.com>
2024-04-04 12:55:34 +0330

    few changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import Footer from "@/components/Modules/Footer/Footer";
 import Navbar from "@/components/Modules/Navbar/Navbar";
 import "@/styles/globals.css";
-import "leaflet/dist/leaflet.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { AppProps } from "next/app";
@@ -14,9 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <NextThemesProvider attribute="class" defaultTheme="light">
         <div className="pt-5 md:pt-8 relative">
           <Navbar />
-          <div className="mx-3 sm:mx-0">
-            <Component {...pageProps} />
-          </div>
+          <Component {...pageProps} />
           <Footer />
         </div>
       </NextThemesProvider>
