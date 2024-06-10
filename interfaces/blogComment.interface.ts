bdd66c59be2b3f6aa95ed52ee17b2f176facd33c
micheal aghaei <aghaeimicheal@gmail.com>
2024-06-10 03:49:47 +0330

    news api

---
+++ interfaces/blogComment.interface.ts
@@ -0,0 +1,9 @@
+interface blogProps {
+  newsId: string | string[] | undefined;
+  userIpAddress: string;
+  title: string;
+  describe: string;
+  userId: string | string[] | undefined;
+  }
+  
+  export type { blogProps };
